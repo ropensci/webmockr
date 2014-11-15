2a9a7b11703070e692e50ba049e927fe041b84ca
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-15 08:51:29 -0800

    added pkg level man file

---
+++ R/webmockr.R
@@ -0,0 +1,7 @@
+#' Stubbing and setting expectations on HTTP requests
+#'
+#' @name webmockr
+#' @docType package
+#' @title Stubbing and setting expectations on HTTP requests
+#' @keywords package
+NULL
