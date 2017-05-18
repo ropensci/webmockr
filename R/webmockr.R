67129cb4c74408df6b4d63fb19950a9ee5bbafc8
Scott Chamberlain <myrmecocystus@gmail.com>
2017-05-18 08:29:19 -0700

    update pkg level man file fix #8

--- R/webmockr.R
+++ R/webmockr.R
@@ -6,4 +6,19 @@
 #' @docType package
 #' @keywords package
 #' @author Scott Chamberlain
+#'
+#' @section Features:
+#' \itemize{
+#'  \item Stubbing HTTP requests at low http client lib level
+#'  \item Setting and verifying expectations on HTTP requests
+#'  \item Matching requests based on method, URI, headers and body
+#'  \item Can support many HTTP libraries, though only \pkg{crul} for now
+#'  \item Integration with testing libraries (coming soon) via `vcr`
+#' }
+#'
+#' @examples
+#' library(webmockr)
+#' stub_request("get", "https://httpbin.org/get")
+#' stub_request("post", url="https://httpbin.org/post")
+#' stub_registry()
 NULL
