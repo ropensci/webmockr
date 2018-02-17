c1595832fdd9dc65e0a73af778fc091990d9f872
Scott Chamberlain <myrmecocystus@gmail.com>
2018-02-17 09:15:13 -0800

    add an example for stub_reigstry_clear

--- R/stub_registry_clear.R
+++ R/stub_registry_clear.R
@@ -3,8 +3,14 @@
 #' Clear all stubs
 #'
 #' @export
-#' @return nothing
+#' @return nothing, well technically an empty list, but
+#' it's not anything useful
 #' @family stub-registry
+#' @examples
+#' (x <- stub_request("get", "https://httpbin.org/get"))
+#' stub_registry()
+#' stub_registry_clear()
+#' stub_registry()
 stub_registry_clear <- function() {
   webmockr_stub_registry$remove_all_request_stubs()
 }
