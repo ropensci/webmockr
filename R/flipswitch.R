e581050e69aa9fd72b02b684a5334d5ea398cce3
Scott Chamberlain <myrmecocystus@gmail.com>
2018-04-16 19:22:49 -0700

    add enabled() method to check if webmockr is enabled

--- R/flipswitch.R
+++ R/flipswitch.R
@@ -5,11 +5,29 @@ webmockr_lightswitch$crul <- FALSE
 #' Enable or disable webmockr
 #'
 #' @export
+#' @details `enable()` enables \pkg{webmockr} for all adapters. 
+#' `disable()` disables \pkg{webmockr} for all adapters.  `enabled()` 
+#' answers whether \pkg{webmockr} is enabled for a given adapter
+#' @return `enable()` and `disable()` invisibly returns booleans for 
+#' each adapter (currently only \pkg{crul}), as a result of running 
+#' enable or disable, respectively, on each [HttpLibAdapaterRegistry]
+#' object. `enabled` returns a single boolean
 #' @param options list of options - ignored for now.
 enable <- function(options = list()) {
   invisible(vapply(http_lib_adapter_registry$adapters, function(z) {
     z$enable()
   }, logical(1)))
+}
+
+#' @export
+#' @rdname enable
+enabled <- function(adapter = "crul") {
+  adapters <- c('crul')
+  if (!adapter %in% adapters) {
+    stop("'adapter' must be in the set ", 
+      paste0(adapters, collapse = ", "))
+  }
+  webmockr_lightswitch$crul
 }
 
 #' @export
