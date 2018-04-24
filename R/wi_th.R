9236a716d6c20d7c1ded2abc965dd75a42bafcd8
Scott Chamberlain <myrmecocystus@gmail.com>
2018-04-24 12:59:59 -0700

    add .list param to wi_th to allow escaping nse workflow

--- R/wi_th.R
+++ R/wi_th.R
@@ -9,6 +9,10 @@
 #' to [lazyeval::lazy_dots()]. accepts the following: query, body,
 #' headers
 #' @param .dots	Used to work around non-standard evaluation
+#' @param .list named list of things, has to be one of 'query', 'body', 
+#' and/or 'headers'. This is an escape hatch to avoid NSE (non-standard
+#' evaluation), so don't pass the same thing to both, e.g. don't pass
+#' 'query' via NSE, and also 'query' to this parameter
 #' @details `with` is a function in the `base` package, so we went with
 #' `wi_th`
 #' @return an object of class `StubbedRequest`, with print method describing
@@ -19,19 +23,24 @@
 #' - query: (list) a named list
 #' - body: various, including character string, list, raw, numeric, etc
 #' - headers: (list) a named list
-wi_th <- function(.data, ...) {
-  wi_th_(.data, .dots = lazyeval::lazy_dots(...))
+wi_th <- function(.data, ..., .list = list()) {
+  wi_th_(.data, .dots = lazyeval::lazy_dots(...), .list = .list)
 }
 
 #' @export
 #' @rdname wi_th
-wi_th_ <- function(.data, ..., .dots) {
+wi_th_ <- function(.data, ..., .dots, .list) {
   assert(.data, 'StubbedRequest')
   tmp <- lazyeval::all_dots(.dots, ...)
   if (length(tmp) == 0) {
     z <- NULL
   } else {
     z <- lapply(tmp, function(x) eval(x$expr))
+  }
+
+  z <- c(z, .list)
+  if (any(duplicated(names(z)))) {
+    stop("can not have duplicated names")
   }
 
   # lint user inputs
