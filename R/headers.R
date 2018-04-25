6d7e6ea503c994f23a9a791837153023f0f94938
Scott Chamberlain <myrmecocystus@gmail.com>
2018-04-25 14:03:37 -0700

    fix duplicate header problem when reading in headers with > 1 value

--- R/headers.R
+++ R/headers.R
@@ -1,21 +1,26 @@
 # headers <- list(`Content-type` = 'application/json', Stuff = "things")
 # normalize_headers(x = headers)
+#
+# headers <- list(`Content-type` = 'application/json', `x-frame-options` = c("SAMEORIGIN", "sameorigin"))
+# normalize_headers(x = headers)
 normalize_headers <- function(x = NULL) {
   if (is.null(x) || length(x) == 0) return(x)
-  res <- Map(function(name, value) {
+
+  res <- list()
+  for (i in seq_along(x)) {
     name <- paste0(
-      vapply(strsplit(as.character(name), '_|-')[[1]], function(w) simple_cap(w), ""),
+      vapply(strsplit(as.character(names(x)[i]), '_|-')[[1]], function(w) simple_cap(w), ""),
       collapse = "-"
     )
     value <- switch(
-      class(value),
-      #when Regexp then value
-      list = if (length(value) == 1) value[[1]] else sort(vapply(value, function(z) as.character(z), "")),
-      as.character(value)
+      class(x[[i]]),
+      list = if (length(x[[i]]) == 1) x[[i]][[1]] else sort(vapply(x[[i]], function(z) as.character(z), "")),
+      if (length(x[[i]]) > 1) paste0(as.character(x[[i]]), collapse = ",") else as.character(x[[i]])
     )
-    list(name, value)
-  }, names(x), unlist(unname(x)))
-  vapply(res, function(z) stats::setNames(z[2], z[1]), list(1))
+    res[[i]] <- list(name, value)
+  }
+
+  unlist(lapply(res, function(z) stats::setNames(z[2], z[1])), FALSE)
 }
 
 simple_cap <- function(x) {
