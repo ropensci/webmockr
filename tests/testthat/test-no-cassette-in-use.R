11161a8a770ca900fb8105d9e93e5dd2cf04ae56
Scott Chamberlain <myrmecocystus@gmail.com>
2018-05-08 06:40:24 -0700

    put vcr back into pkg

--- tests/testthat/test-no-cassette-in-use.R
+++ tests/testthat/test-no-cassette-in-use.R
@@ -1,26 +1,26 @@
-# context("no_cassette_in_use")
+context("no_cassette_in_use")
 
-# library(vcr)
-# dir <- tempdir()
-# invisible(vcr_configure(dir = dir))
-#
-# test_that("no cassette in use behaves as expected", {
-#   crul::mock()
-#   x <- crul::HttpClient$new(url = "https://httpbin.org")
-#
-#   # when no cassette in use, we get expected vcr error
-#   expect_error(
-#     x$get("get"),
-#     "There is currently no cassette in use"
-#   )
-# })
-#
-#
-# # cleanup
-# unlink(file.path(vcr_configuration()$dir, "turtle.yml"))
-#
-# # reset configuration
-# vcr_configure_reset()
-#
-# # unload vcr
-# unloadNamespace("vcr")
+library(vcr)
+dir <- tempdir()
+invisible(vcr_configure(dir = dir))
+
+test_that("no cassette in use behaves as expected", {
+  crul::mock()
+  x <- crul::HttpClient$new(url = "https://httpbin.org")
+
+  # when no cassette in use, we get expected vcr error
+  expect_error(
+    x$get("get"),
+    "There is currently no cassette in use"
+  )
+})
+
+
+# cleanup
+unlink(file.path(vcr_configuration()$dir, "turtle.yml"))
+
+# reset configuration
+vcr_configure_reset()
+
+# unload vcr
+unloadNamespace("vcr")
