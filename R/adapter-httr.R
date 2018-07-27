45e450e05ec8880168e5bb4e7d1ba842d50eca98
Scott Chamberlain <myrmecocystus@gmail.com>
2018-07-26 18:24:20 -0700

    tweak httr adapter

--- R/adapter-httr.R
+++ R/adapter-httr.R
@@ -311,7 +311,7 @@ build_httr_response <- function(req, resp) {
     request = req,
     handle = NA
   )
-  if ("content-type" %in% lst$headers) {
+  if ("content-type" %in% names(lst$headers)) {
     lst$headers$`Content-Type` <- lst$headers$`content-type`
     lst$headers$`content-type` <- NULL
   } 
