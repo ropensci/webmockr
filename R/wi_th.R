e8fb9fe428549c41f782a1e9c4401eddf76b0a39
Scott Chamberlain <myrmecocystus@gmail.com>
2019-02-05 13:23:12 -0800

    #60 fixes for wi_th and to_return
    remove lazyeval as not needed anymore given changes in these fxns
    change both fxns to not use lazyeval anymore, instead use plain ol list(...)
    add .list param to to_return to match wi_th
    add additioanl tests for actual use of stubbing http requesets with httr/crul in test_that blocks
    remove to_return_ and wi_th_ underscore methods
    add additional assertions to to_return and wi_th
    update codemeta.json
    bump patch version

--- R/wi_th.R
+++ R/wi_th.R
@@ -5,20 +5,18 @@
 #' @export
 #' @param .data input. Anything that can be coerced to a `StubbedRequest` class
 #' object
-#' @param ... Comma separated list of variable names, passed on
-#' to [lazyeval::lazy_dots()]. accepts the following: query, body,
-#' headers
-#' @param .dots	Used to work around non-standard evaluation
-#' @param .list named list of things, has to be one of 'query', 'body',
-#' and/or 'headers'. This is an escape hatch to avoid NSE (non-standard
-#' evaluation), so don't pass the same thing to both, e.g. don't pass
-#' 'query' via NSE, and also 'query' to this parameter
+#' @param ... Comma separated list of named variables. accepts the following: 
+#' `query`, `body`, `headers`.
+#' @param .list named list, has to be one of 'query', 'body',
+#' and/or 'headers'. An alternative to passing in via `...`. Don't pass the 
+#' same thing to both, e.g. don't pass 'query' to `...`, and also 'query' to 
+#' this parameter
 #' @details `with` is a function in the `base` package, so we went with
 #' `wi_th`
 #' @return an object of class `StubbedRequest`, with print method describing
 #' the stub
-#' @note see examples in [stub_request()]
-#' @details Values for status, body, and headers:
+#' @note see more examples in [stub_request()]
+#' @details Values for query, body, and headers:
 #'
 #' - query: (list) a named list
 #' - body: various, including character string, list, raw, numeric, etc
@@ -46,38 +44,25 @@
 #' wi_th(req, headers = list(`User-Agent` = "webmockr/v1", hello="world"))
 #'
 #'
-#' # .list NSE escape hatch
+#' # .list - pass in a named list instead
 #' wi_th(req, .list = list(body = list(foo = "bar")))
 wi_th <- function(.data, ..., .list = list()) {
-  wi_th_(.data, .dots = lazyeval::lazy_dots(...), .list = .list)
-}
-
-#' @export
-#' @rdname wi_th
-wi_th_ <- function(.data, ..., .dots, .list) {
-  assert(.data, 'StubbedRequest')
-  tmp <- lazyeval::all_dots(.dots, ...)
-  if (length(tmp) == 0) {
-    z <- NULL
-  } else {
-    z <- lapply(tmp, function(x) eval(x$expr))
-  }
-
+  assert(.data, "StubbedRequest")
+  assert(.list, "list")
+  z <- list(...)
+  if (length(z) == 0) z <- NULL
   z <- c(z, .list)
-  if (any(duplicated(names(z)))) {
-    stop("can not have duplicated names")
+  if (
+    !any(c("query", "body", "headers") %in% names(z)) &&
+    length(z) != 0
+  ) {
+    stop("'wi_th' only accepts query, body, headers")
   }
-
-  # lint user inputs
-  assert(z$query, 'list')
-  if (!all(hz_namez(z$query))) {
-    stop("'query' must be a named list")
-  }
-  assert(z$headers, 'list')
-  if (!all(hz_namez(z$headers))) {
-    stop("'headers' must be a named list")
-  }
-
+  if (any(duplicated(names(z)))) stop("can not have duplicated names")
+  assert(z$query, "list")
+  if (!all(hz_namez(z$query))) stop("'query' must be a named list")
+  assert(z$headers, "list")
+  if (!all(hz_namez(z$headers))) stop("'headers' must be a named list")
   .data$with(
     query = z$query,
     body = z$body,
---
+++ tests/testthat/test-within_test_that_blocks.R
@@ -0,0 +1,55 @@
+context("within test_that blocks: httr")
+library("httr")
+test_that("httr: without pipe", {
+  httr_mock()
+  enable()
+
+  dat_json <- '{"foo":"bar"}'
+  stub <- stub_request("get", uri = "https://httpbin.org/get")
+  to_return(stub,
+    body = dat_json,
+    headers = list("Content-Type" = "application/json; charset=utf-8")
+  )
+  res <- GET("https://httpbin.org/get")
+  expect_true(inherits(res, "response"))
+  expect_is(content(res), "list")
+  expect_named(content(res), "foo")
+  expect_equal(content(res)$foo, "bar")
+  disable()
+  httr_mock(FALSE)
+})
+
+test_that("httr: with pipe", {
+  enable()
+  dat_json <- '{"foo":"bar"}'
+  stub <- stub_request("get", uri = "https://httpbin.org/get") %>%
+    to_return(body = dat_json,
+      headers = list("Content-Type" = "application/json; charset=utf-8"))
+  res <- GET("https://httpbin.org/get")
+  expect_true(inherits(res, "response"))
+  expect_is(content(res), "list")
+  expect_named(content(res), "foo")
+  expect_equal(content(res)$foo, "bar")
+  disable()
+})
+unloadNamespace("httr")
+
+context("within test_that blocks: crul")
+library("crul")
+test_that("crul works", {
+  enable()
+  dat_json <- '{"foo":"bar"}'
+  stub <- stub_request("get", uri = "https://httpbin.org/get")
+  to_return(stub,
+    body = dat_json,
+    headers = list("Content-Type" = "application/json; howdy")
+  )
+  res <- crul::HttpClient$new("https://httpbin.org")$get("get")
+  expect_true(inherits(res, "HttpResponse"))
+  expect_is(res$parse("UTF-8"), "character")
+  expect_is(jsonlite::fromJSON(res$parse("UTF-8")), "list")
+  expect_named(jsonlite::fromJSON(res$parse("UTF-8")), "foo")
+  expect_equal(jsonlite::fromJSON(res$parse("UTF-8"))$foo, "bar")
+  disable()
+})
+unloadNamespace("crul")
