3e2d57648dcb000f3e1cd1b48787dda860920272
Aaron Wolen <aaron@wolen.com>
2019-12-16 11:02:46 -0600

    improve pluck_body()'s docs

--- R/pluck_body.R
+++ R/pluck_body.R
@@ -1,7 +1,15 @@
 #' Extract the body from an HTTP request
+#' 
+#' Returns an appropriate representation of the data contained within a request
+#' body based on its encoding.
+#' 
 #' @export
-#' @param x an unexecuted crul \emph{or} httr request object
-#' @return a list containing ... or `NULL` if the request does not contain a 
+#' @param x an unexecuted crul *or* httr request object
+#' @return one of the following:
+#' - `NULL` if the request is not associated with a body
+#' - list containing the multipart-encoded body
+#' - character vector with the JSON- or raw-encoded body, or upload form
+
 pluck_body <- function(x) {
   assert_request(x)
   if (is_body_empty(x)) return(NULL)
@@ -21,7 +29,8 @@ pluck_body <- function(x) {
     return(rawToChar(x$options$postfields))
   } else {
     stop("couldn't fetch request body; file an issue at \n",
-          "  https://github.com/ropensci/webmockr/issues/")
+         "  https://github.com/ropensci/webmockr/issues/",
+         call. = FALSE)
   }
 }
 
