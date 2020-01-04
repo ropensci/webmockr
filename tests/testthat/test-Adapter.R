9fdf21a467795fa3a7594ca38ae28a1dcbf2e0fb
Aaron Wolen <aaron@wolen.com>
2020-01-04 15:50:05 -0600

    add tests for Adapter class

---
+++ tests/testthat/test-Adapter.R
@@ -0,0 +1,9 @@
+context("Adapter class")
+
+test_that("Adapter class can't be instantiated", {
+  expect_is(Adapter, "R6ClassGenerator")
+  expect_error(
+    Adapter$new(), 
+    "Adapter parent class should not be called directly"
+  )
+})
