2a8e5bc5a2fce011ec0d937b9caac3da92b03471
Hadley Wickham <h.wickham@gmail.com>
2025-07-02 16:36:03 -0500

    Eliminate partial match warnings (#147)
    I'm reasonably certain that this is correct based on my reading on httr2 and httr source code.
    
    Co-authored-by: Scott Chamberlain <sckott@fastmail.com>

--- R/pluck_body.R
+++ R/pluck_body.R
@@ -22,7 +22,7 @@ pluck_body <- function(x) {
     return(x$fields)
 
     # json/raw-encoded body
-  } else if (!is.null(x$options$postfields) && is.raw(x$options$postfields)) {
+  } else if (has_name(x$options, "postfields") && is.raw(x$options$postfields)) {
     return(rawToChar(x$options$postfields))
 
     # upload not in a list
@@ -49,5 +49,6 @@ assert_request <- function(x) {
 
 is_body_empty <- function(x) {
   is.null(x$fields) &&
-    (is.null(x$options$postfieldsize) || x$options$postfieldsize == 0L)
+    (!has_name(x$options, "postfieldsize_large") || x$options$postfieldsize_large == 0L) &&
+    (!has_name(x$options, "postfieldsize") || x$options$postfieldsize == 0L)
 }
